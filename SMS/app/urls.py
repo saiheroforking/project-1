98f947665034530714cb95f358b51228a5857940
saiheroforking <notlasaikanna09@gmail.com>
2025-12-27 16:17:22 +0530

    files are added

--- SMS/app/urls.py
+++ SMS/app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", views.student_form, name="create"),
     path("success/", views.success, name="success"),
     path("Show/",views.Show,name="Show"),
-]   
+    path('update/<int:pk>/', views.update_details, name='update_details'),
+    path('delete/<int:id>/', views.delete_student, name='delete_student'),
+]
--- SMS/app/views.py
+++ SMS/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from .forms import StudentForm
 from .models import Student
 
@@ -22,3 +22,20 @@ def Show(request):
     show_details = Student.objects.all()
     return render(request,"Show_Details.html",{"Show_details":show_details})
 
+def update_details(request, pk):
+    student = get_object_or_404(Student, id=pk)
+
+    if request.method == "POST":
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('app:Show')
+    else:
+        form = StudentForm(instance=student)
+
+    return render(request, 'update_details.html', {'form': form})
+
+def delete_student(request, id):
+    student = get_object_or_404(Student, id=id)
+    student.delete()
+    return redirect('app:Show')
